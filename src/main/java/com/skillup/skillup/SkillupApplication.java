425760596b945d2d4350e24df70ccbd149762781
zaneHawkins <70725038+zaneHawkins@users.noreply.github.com>
2024-05-11 00:47:16 -0400

    Init Project

---
+++ src/main/java/com/skillup/skillup/SkillupApplication.java
@@ -0,0 +1,13 @@
+package com.skillup.skillup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillupApplication.class, args);
+	}
+
+}
