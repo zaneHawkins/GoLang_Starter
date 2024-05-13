3db2ea90c97a1d0030ce9a718fe834804b18987f
zaneHawkins <70725038+zaneHawkins@users.noreply.github.com>
2024-05-12 23:59:11 -0400

    Add middleware to validate token

--- src/api/v1/services/authentication.go
+++ src/api/v1/services/authentication.go
@@ -7,6 +7,7 @@ import (
 	"github.com/volatiletech/sqlboiler/v4/boil"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 	"golang.org/x/crypto/bcrypt"
+	H "src/handler"
 	M "src/models"
 	T "src/types"
 	U "src/utils"
@@ -16,14 +17,14 @@ func SignUp(db boil.ContextExecutor, ctx context.Context, user *M.User) (*T.Toke
 	// Encrypt Password
 	pass, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
-		return nil, U.ServiceError(fiber.StatusInternalServerError, "Password Encryption failed", err)
+		return nil, H.ServiceError(fiber.StatusInternalServerError, "Password Encryption failed", err)
 	}
 	user.Password = string(pass)
 
 	// Insert user
 	err = user.Insert(ctx, db, boil.Infer())
 	if err != nil {
-		return nil, U.ServiceError(fiber.StatusConflict, "Email already has an account", err)
+		return nil, H.ServiceError(fiber.StatusConflict, "Email already has an account", err)
 	}
 
 	// Get New JWT tokens
@@ -35,13 +36,13 @@ func Login(db boil.ContextExecutor, ctx context.Context, loginRequest *T.LoginRe
 	// Find User by email
 	user, err := M.Users(qm.Where("email = ?", loginRequest.Email)).One(ctx, db)
 	if err != nil {
-		return nil, U.ServiceError(fiber.StatusNotFound, "User not found", err)
+		return nil, H.ServiceError(fiber.StatusNotFound, "User not found", err)
 	}
 
 	// Check Password matches
 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginRequest.Password))
 	if err != nil {
-		return nil, U.ServiceError(fiber.StatusForbidden, "Password did not match", errors.New("Incorrect Password"))
+		return nil, H.ServiceError(fiber.StatusForbidden, "Password did not match", errors.New("Incorrect Password"))
 	}
 
 	// Get New JWT tokens
